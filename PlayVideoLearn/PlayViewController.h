59c14a5f759c2ad5d81e86c6874c2bae0254798e
Gaojuan <2769442614@qq.com>
2017-05-10 16:31:49 +0800

    First commit

---
+++ PlayVideoLearn/PlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayViewController.h
+//  MiYun360
+//
+//  Created by Gaojuan on 2017/5/10.
+//  Copyright © 2017年 AUNotBad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayViewController : UIViewController
+
+@end
---
+++ PlayVideoLearn/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  PlayVideoLearn
+//
+//  Created by Gaojuan on 2017/5/10.
+//  Copyright © 2017年 Gaojuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.title = @"jjjj";
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
